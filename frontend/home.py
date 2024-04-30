7db98e8d8befb7ea30b8745c3034e36f814b5d0d
Aseel <fatayerjiaseel@gmail.com>
2024-05-01 00:54:45 +0300

    created test screen

--- frontend/home.py
+++ frontend/home.py
@@ -1,14 +1,25 @@
-import kivy
+# from kivymd.app import MDApp
 from kivy.app import App
 from kivy.lang import Builder
+
+from kivy.uix.screenmanager import ScreenManager, Screen
 # import kivysome
 # kivysome.enable( "https://kit.fontawesome.com/3466402afb.js", group=kivysome.FontGroup.REGULAR)
+class MainWindow(Screen):
+    pass
 
-class MyApp(App):
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+class SilentLearning(App):
 
     def build(self):
-        return Builder.load_file('components/bbar.kv')
+        
+        return Builder.load_file('components/main.kv')
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    SilentLearning().run()

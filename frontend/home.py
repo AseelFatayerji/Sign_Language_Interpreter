380094147685af75e23e34aade792ac0e094122c
Aseel <fatayerjiaseel@gmail.com>
2024-05-01 14:50:16 +0300

    Set screen size

--- frontend/home.py
+++ frontend/home.py
@@ -1,13 +1,16 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-# import kivysome
-# kivysome.enable( "https://kit.fontawesome.com/3466402afb.js", group=kivysome.FontGroup.REGULAR)
+from kivy.core.window import Window
+
 class NavBar(BoxLayout):
     pass
 class SilentLearning(MDApp):
 
     def build(self):
+        Window.size = (260, 500)
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_pallet = "#71D4CC"
         return Builder.load_file('components/navbar.kv')
 
 

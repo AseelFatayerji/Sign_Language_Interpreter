6db7a37f00411d0e784fe950950061b351ef6c29
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-07 22:39:48 +0300

    Created test login page

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         backgroundColor: Color.fromARGB(255, 37, 35, 38),
+         backgroundColor: const Color.fromARGB(255, 37, 35, 38),
       ),
       body: Center(
         child: TextButton(

c680835cf03458d7918fdadeafae284de1138bf5
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-07 22:09:56 +0300

    Fixed typo

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key,}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key,}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -20,9 +20,8 @@ class MyApp extends StatelessWidget {
           items: const [
             BottomNavigationBarItem(
                 icon: Icon(Icons.translate), label: "Set Language", ),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.video_call), label: "Translate"),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home",activeIcon: HomePage()),
+            BottomNavigationBarItem(icon: Icon(Icons.video_call), label: "Translate"),
           ],
         ),
       ),

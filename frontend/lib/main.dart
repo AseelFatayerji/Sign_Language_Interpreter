6cdb0b01181366846557c0b4db578eedff5e9799
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-08 02:54:27 +0300

    Created navbar component

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-import 'login.dart';
-import 'translate.dart';
-import 'language.dart';
+import 'navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,38 +9,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    int myIndex = 1;
-    List<Widget> pages = const [
-      LanguagePage(),
-      HomePage(),
-      TranslationPage(),
-    ];
     return MaterialApp(
       title: 'Silent Learning',
-      home: Scaffold(
-        body: IndexedStack(
-          index: myIndex,
-          children: pages,
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: const Color.fromARGB(255, 37, 37, 37),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: const Color.fromARGB(255, 146, 146, 146),
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.translate),
-              label: "Set Language",
-            ),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.video_call), label: "Translate"),
-          ],
-          onTap: (int index) {
-            myIndex = index;
-          },          
-          currentIndex: myIndex,
-        ),
-      ),
+      home: Navbar(),
     );
   }
 }
---
+++ frontend/lib/navbar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'login.dart';
+import 'translate.dart';
+import 'language.dart';
+
+class Navbar extends StatefulWidget {
+  @override
+  _NavbarState createState() =>
+      _NavbarState();
+}
+
+class _NavbarState
+    extends State<Navbar> {
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    int myIndex = 1;
+    List<Widget> pages = const [
+      LanguagePage(),
+      HomePage(),
+      TranslationPage(),
+    ];
+    return Scaffold(
+        body: IndexedStack(
+          index: myIndex,
+          children: pages,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 37, 37, 37),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: const Color.fromARGB(255, 146, 146, 146),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.translate),
+              label: "Set Language",
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.video_call), label: "Translate"),
+          ],
+          onTap: (int index) {
+            myIndex = index;
+          },          
+          currentIndex: myIndex,
+        ),
+      );
+  }
+}

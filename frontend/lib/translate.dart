995500918c1cf700d3e40b8cb6fa194fec2b7d14
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-07 22:39:15 +0300

    Created test translte page

---
+++ frontend/lib/translate.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key,}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+         backgroundColor: Color.fromARGB(255, 37, 35, 38),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {},
+          child: const Text('Start'),
+          
+        ),
+      ),
+    );
+  }
+}

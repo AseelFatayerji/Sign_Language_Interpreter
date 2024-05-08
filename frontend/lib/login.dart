5ae73472c704dda1b5e0c01beadc81c06f965a1c
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-08 17:50:11 +0300

    Added login widgets

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key,}) : super(key: key);
+  const LoginPage({
+    Key? key,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         backgroundColor: const Color.fromARGB(255, 37, 35, 38),
+        backgroundColor: const Color.fromARGB(255, 37, 35, 38),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {},
-          child: const Text('Start'),
-          
-        ),
-      ),
+      backgroundColor: Colors.black38,
+      body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 238, 238, 238),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+              border: Border.all(color: Colors.transparent),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: const Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          )),
     );
   }
 }

fc1fdc211943a908eb05cd869fff7b895fe1b338
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-10 18:05:57 +0300

    Added title to splash screen

--- frontend/lib/splash.dart
+++ frontend/lib/splash.dart
@@ -6,8 +6,20 @@ class SplashScreen extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Image(
-      image: AssetImage("assets/logo-transparent.png"),
-    );
+    return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/logo-transparent.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const Text(
+          'Silent Learning',
+          style: TextStyle(
+            color: Color.fromARGB(255, 113, 212, 204),
+            fontWeight: FontWeight.bold,
+            fontSize: 22.0,
+          ),
+        ));
   }
 }

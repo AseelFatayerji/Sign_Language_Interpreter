89386ad1cadbf09f42a6f331ffc629fdd92c2bf3
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-10 15:19:58 +0300

    Redeign login test

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -13,34 +13,66 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.black38,
       body: Align(
           alignment: Alignment.bottomCenter,
-          child: Container(
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 238, 238, 238),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
-              ),
-              border: Border.all(color: Colors.transparent),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: const Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Stack(
+              alignment: Alignment.topCenter,
+              clipBehavior: Clip.none,
               children: [
-                Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  height: 500,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 238, 238, 238),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                    border: Border.all(color: Colors.transparent),
+                  ),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '1.Allow camera use.\n2.Stand in front of the camera with both hands visible.\n3.Click on the camera icon.\n4.Happy signing :).\n5.Change the selected language in the language section (Optional).',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 100,
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(
-                  height: 30,
-                ),
-              ],
-            ),
-          )),
+                Positioned(
+                  top: -80,
+                  child: Container(
+                    decoration: const ShapeDecoration(
+                        color: Color.fromARGB(255, 238, 238, 238),
+                        shape: CircleBorder()),
+                    height: 160,
+                    width: 160,
+                    padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    child: const Image(
+                        alignment: Alignment.center,
+                        image: AssetImage("assets/logo-transparent.png"),),
+                  ),
+                )
+              ])),
     );
   }
 }

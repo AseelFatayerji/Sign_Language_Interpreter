f4c6186b9d9334fcff7fbdbbf8be8e10039aebb3
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-10 14:13:07 +0300

    Added logo container

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -14,44 +14,61 @@ class HomePage extends StatelessWidget {
       body: Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 238, 238, 238),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
-              ),
-              border: Border.all(color: Colors.transparent),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: const Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'How to use',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                Container(
+                  decoration: const ShapeDecoration(
+                      color: Color.fromARGB(255, 238, 238, 238),
+                      shape: CircleBorder()),
+                  height: 150,
+                  width: 150,
+                  // child: Image(image: AssetImage()),
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 238, 238, 238),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                    border: Border.all(color: Colors.transparent),
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  '1.Allow camera use.\n2.Stand in front of the camera with both hands visible.\n3.Click on the camera icon.\n4.Happy signing :).\n5.Change the selected language in the language section (Optional).',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'How to use',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        '1.Allow camera use.\n2.Stand in front of the camera with both hands visible.\n3.Click on the camera icon.\n4.Happy signing :).\n5.Change the selected language in the language section (Optional).',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 100,
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 250,
-                ),
-              ],
-            ),
-          )),
+                )
+              ]))),
     );
   }
 }

249707bfcb611c95c41677190c4e1086c1d4d48d
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-10 15:24:25 +0300

    Created test splash screen

---
+++ frontend/lib/splash.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 37, 35, 38),
+      ),
+      backgroundColor: Colors.black38,
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white54,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: Colors.transparent),
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: const Text(
+            'Translation',
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

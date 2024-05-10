775477d013ac78fe4856dc239ad639d80f3e560b
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-10 18:43:30 +0300

    Created  user model

---
+++ backend/models/user_model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: "Name is required",
+  },
+  email: {
+    type: String,
+    require: "Email is required",
+    unique: true,
+  },
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

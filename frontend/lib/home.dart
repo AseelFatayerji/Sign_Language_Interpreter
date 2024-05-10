1c97c83a8993eb032b0dec257baa6e5282fd0417
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-10 14:54:42 +0300

    Fixed logo position

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -13,51 +13,62 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.black38,
       body: Align(
           alignment: Alignment.bottomCenter,
-          child: Stack(children: [
-            Container(
-              decoration: const ShapeDecoration(
-                  color: Color.fromARGB(255, 0, 0, 0), shape: CircleBorder()),
-              height: 150,
-              width: 150,
-              // child: Image(image: AssetImage()),
-            ),
-            Container(
-              height: 500,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 238, 238, 238),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
+          child: Stack(
+              alignment: Alignment.topCenter,
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 500,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 238, 238, 238),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                    border: Border.all(color: Colors.transparent),
+                  ),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'How to use',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '1.Allow camera use.\n2.Stand in front of the camera with both hands visible.\n3.Click on the camera icon.\n4.Happy signing :).\n5.Change the selected language in the language section (Optional).',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 100,
+                      ),
+                    ],
+                  ),
                 ),
-                border: Border.all(color: Colors.transparent),
-              ),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'How to use',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                Positioned(
+                  top: -80,
+                  child: Container(
+                    decoration: const ShapeDecoration(
+                        color: Color.fromARGB(255, 238, 238, 238),
+                        shape: CircleBorder()),
+                    height: 150,
+                    width: 150,
+                    // child: Image(image: AssetImage()),
                   ),
-                  Text(
-                    '1.Allow camera use.\n2.Stand in front of the camera with both hands visible.\n3.Click on the camera icon.\n4.Happy signing :).\n5.Change the selected language in the language section (Optional).',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 100,
-                  ),
-                ],
-              ),
-            )
-          ])),
+                )
+              ])),
     );
   }
 }

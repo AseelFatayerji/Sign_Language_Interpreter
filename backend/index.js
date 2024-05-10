7fc498b56ccc7c8b5b7c02385f6bbfef4239cc91
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-10 18:49:55 +0300

    Created added routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { connected } = require("./config/db_config")
 const userRouter = require("./routes/user_routes");
+const authRouter = require("./routes/auth_routes");
+const protected = require("./routes/protected_routes")
 require("dotenv").config();
 
 connected();
@@ -8,6 +10,8 @@ connected();
 const app = express();
 
 app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/verify", protected);
 
 app.listen(3001, () => {
   console.log("server is running on port 3001");

5642da730635e49ce9c240fac9d95f49db15ca58
AseelFatayerji <fatayerjiaseel@gmail.com>
2024-05-14 14:20:16 +0300

    Created admin panel

--- frontend/lib/adminnav.dart
+++ frontend/lib/adminnav.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 import 'language.dart';
 import 'translate.dart';
+import 'teaching.dart';
+import 'profile.dart';
+import 'userList.dart';
+import 'admin.dart';
 
 class AdminNav extends StatefulWidget {
   @override
@@ -9,59 +13,110 @@ class AdminNav extends StatefulWidget {
 }
 
 class _AdminNavState extends State<AdminNav> {
-  int _selectedIndex = 2;
+  int _selectedIndex = 1;
   final List<Widget> _screens = <Widget>[
-    const LanguagePage(),
     const HomePage(),
+    AdminPanel(),
+    ProfileScreen(),
+    UserList(),
+    ModelUpdate(),
+    const LanguagePage(),
     TranslationPage(),
   ];
-
-  void _onItemTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 37, 35, 38),
+        iconTheme: IconThemeData(color: Colors.white),
       ),
+      drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              ListTile(
+                leading: const Icon(
+                  Icons.home,
+                ),
+                title: const Text('Home'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 0;
+                  });
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.admin_panel_settings,
+                ),
+                title: const Text('Admin Panel'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 1;
+                  });
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.person,
+                ),
+                title: const Text('Profile'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 2;
+                  });
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.people,
+                ),
+                title: const Text('User List'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 3;
+                  });
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.add_box_rounded,
+                ),
+                title: const Text('Add words'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 4;
+                  });
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.translate,
+                ),
+                title: const Text('Language Select'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 5;
+                  });
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.video_call,
+                ),
+                title: const Text('Translate'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 6;
+                  });
+                },
+              )
+            ],
+          )),
       body: Center(
         child: _screens.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color.fromARGB(255, 37, 35, 38),
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: _selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        iconSize: 35,
-        items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.translate,
-                  ),
-                  label: 'Language',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.home,
-                  ),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.video_call,
-                  ),
-                  label: 'Transalte',
-                ),
-              ],
-        onTap: _onItemTap,
-        selectedItemColor: Colors.white,
-        unselectedFontSize: 13.0,
-        unselectedItemColor: const Color.fromARGB(255, 144, 144, 114),
       ),
     );
   }

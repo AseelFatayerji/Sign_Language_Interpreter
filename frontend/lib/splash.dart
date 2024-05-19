52ebbce6bf802cf87e0b18482e3991317185ba13
Aseel <fatayerjiaseel@gmail.com>
2024-05-19 09:48:10 +0300

    Cleaned up code

--- frontend/lib/splash.dart
+++ frontend/lib/splash.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
   @override
   Widget build(BuildContext context) {
     return Container(

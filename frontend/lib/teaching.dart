0c8ba39e161a9cbeca7dd890bdc0d19bb48d2c34
Aseel <fatayerjiaseel@gmail.com>
2024-05-20 01:41:13 +0300

    updated model update

--- frontend/lib/teaching.dart
+++ frontend/lib/teaching.dart
@@ -1,20 +1,28 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/main.dart';
+import 'package:translator/translator.dart';
+
 import 'package:http/http.dart' as http;
 import 'global.dart' as global;
 
-class ModelUpdate extends StatefulWidget {
-  const ModelUpdate({super.key});
+class ModulUpdate extends StatefulWidget {
+  const ModulUpdate({super.key});
 
   @override
-  State<ModelUpdate> createState() => _ModelUpdateState();
+  ModelState createState() => ModelState();
 }
 
-class _ModelUpdateState extends State<ModelUpdate> {
+class ModelState extends State<ModulUpdate> {
+  final translator = GoogleTranslator();
+  late Timer timer;
+  bool isCapturing = false;
+  var btn = "Start";
+
   CameraImage? cameraImage;
   CameraController? controller;
   String output = "Translation";
@@ -22,51 +30,85 @@ class _ModelUpdateState extends State<ModelUpdate> {
   @override
   void initState() {
     super.initState();
+    translator.translate(output, to: global.language).then((result) {
+      setState(() {
+        output = result.text;
+      });
+    });
+
     loadCamera();
   }
 
+  @override
+  void dispose() {
+    controller!.dispose();
+    timer.cancel();
+    super.dispose();
+  }
+
   loadCamera() {
-    controller = CameraController(camera!.first, ResolutionPreset.veryHigh);
-    controller!.initialize().then((value) {
-      if (!mounted) {
+    controller = CameraController(camera!.last, ResolutionPreset.veryHigh);
+    controller!.initialize();
+  }
+
+  startTimer() {
+    if (btn == "Start") {
+      setState(() {
+        btn = "Stop";
         return;
-      } else {
+      });
+      timer = Timer.periodic(const Duration(seconds: 3), (timer) async {
+        if (!isCapturing) {
+          try {
+            XFile? imageFile = await controller!.takePicture();
+            getPredictions(File(imageFile.path));
+          } catch (err) {
+            debugPrint("Error capturing/sending image: $err");
+          }
+        }
+      });
+    } else {
+      setState(() {
+        btn = "Start";
+        return;
+      });
+      timer.cancel();
+    }
+  }
+
+  getPredictions(File imageFile) async {
+    var apiUrl = Uri.parse('http://${global.ipv4}:3001/oneHand');
+    var request = http.MultipartRequest('POST', apiUrl);
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'image',
+        imageFile.path,
+      ),
+    );
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      setState(() {
         setState(() {
-          controller!.startImageStream((ImageStream) {
-            cameraImage = ImageStream;
-            // Timer.periodic(Duration(seconds: 1), (Timer t) {
-            //   debugPrint("hi");
-            // });
+            output = "Upload Successful";
           });
-        });
-      }
-    });
+      });
+    } else {
+      debugPrint("Failed to upload image. Error: ${response.reasonPhrase}");
+    }
   }
 
-  getPredictions(image) async {
-    debugPrint(image);
-    // List<int> imageBytes = await image.readAsBytes();
-    // final resp =
-    //     await http.post(Uri.parse('http://${global.ipv4}:8000/translate'),body: );
-    // if (resp.statusCode == 200) {
-    //   setState(() {
-    //     final Map<String, dynamic> json = jsonDecode(resp.body);
-    //     debugPrint(json.toString());
-    //   });
-    // }
-  }
-
+  @override
   Widget build(BuildContext context) {
+    if (controller == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+        body: Stack(
       children: [
-        Text(
-          output,
-          style: TextStyle(fontSize: 16, color: Colors.black),
-        ),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.7,
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           child: !controller!.value.isInitialized
               ? Container()
@@ -74,7 +116,46 @@ class _ModelUpdateState extends State<ModelUpdate> {
                   aspectRatio: controller!.value.aspectRatio,
                   child: CameraPreview(controller!),
                 ),
-        )
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6),
+              decoration: BoxDecoration(
+                  color: Colors.white60,
+                  borderRadius: BorderRadius.circular(5)),
+              child: Text(
+                output,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                startTimer();
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 113, 212, 204),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40.0, vertical: 2),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                btn,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ],
     ));
   }

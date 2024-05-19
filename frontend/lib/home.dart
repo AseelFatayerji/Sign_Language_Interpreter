551c9ec0d0a3ff4fd27e728124c3058bd7a2a955
Aseel <fatayerjiaseel@gmail.com>
2024-05-19 03:04:55 +0300

    updated icons

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
                       CarouselSlider(
                           items: titles.map((e) {
                             return Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text(
                                   e,

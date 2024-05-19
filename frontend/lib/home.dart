7a2504ef0a43ade4690c511cda5ca1696f95ec44
Aseel <fatayerjiaseel@gmail.com>
2024-05-19 09:52:51 +0300

    Cleaned up code

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});

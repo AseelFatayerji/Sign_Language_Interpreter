647542e232052c11e0b8b7838510aad9493dce65
Aseel <fatayerjiaseel@gmail.com>
2024-05-19 03:12:19 +0300

    updated spacing

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
                       CarouselSlider(
                           items: titles.map((e) {
                             return Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: [
                                 Text(
                                   e,

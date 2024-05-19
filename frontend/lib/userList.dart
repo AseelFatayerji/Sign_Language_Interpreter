5cf27ee70560a3cb827a905b742ef6a640a44def
Aseel <fatayerjiaseel@gmail.com>
2024-05-19 09:55:11 +0300

    Cleaned up code

--- frontend/lib/userList.dart
+++ frontend/lib/userList.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'global.dart' as global;
 
